fc442d201b6693d7507c9d76599f713ad77d79c1
BRADDTELES <braddteles@gmail.com>
2024-09-05 15:39:23 -0300

    insenrindo UtilDate na pasta util

--- app/src/main/java/com/example/appeventos/MainActivity.java
+++ app/src/main/java/com/example/appeventos/MainActivity.java
@@ -1,19 +1,40 @@
 package com.example.appeventos;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.appeventos.util.UtilDate;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nomeEditText;
+    TextView dataEscolhidaTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nomeEditText = findViewById(R.id.nomeEditText);
+        dataEscolhidaTextView = findViewById(R.id.dataEscolhidaTextView);
+    }
+
+    public void escolherDataButtonAction(View view) {
+
+        UtilDate utilDate = new UtilDate();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                month++;
+                dataEscolhidaTextView.setText("" + dayOfMonth + "/" + month + "/" + year);
+            }
+        }, utilDate.getAnoAtual(), utilDate.getMesAtual(), utilDate.getDiaAtual());
+        datePickerDialog.show();
     }
 }
