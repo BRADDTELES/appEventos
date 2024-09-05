d88b1f0b39e9e3ef969b95bacb4423a9b46f9fb7
BRADDTELES <braddteles@gmail.com>
2024-09-05 17:44:33 -0300

    Adicionando TimePicker, códigos e ajustando layout de button e textview (não finalizado)

--- app/src/main/java/com/example/appeventos/MainActivity.java
+++ app/src/main/java/com/example/appeventos/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.appeventos;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.appeventos.util.UtilDate;
+import com.example.appeventos.util.UtilTime;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText nomeEditText;
     TextView dataEscolhidaTextView;
+    TextView horaSelecionadaTextView;
+    Button selecionarHoraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
         nomeEditText = findViewById(R.id.nomeEditText);
         dataEscolhidaTextView = findViewById(R.id.dataEscolhidaTextView);
+
+        horaSelecionadaTextView = findViewById(R.id.horaSelecionadaTextView);
+        selecionarHoraButton = findViewById(R.id.selecionarHoraButton);
+//        horaSelecionadaTextView.setText("Hora selecionada: Nenhuma");
+//        selecionarHoraButton.setText("Selecionar hora inicial");
     }
 
     public void escolherDataButtonAction(View view){
@@ -36,5 +47,25 @@ public class MainActivity extends AppCompatActivity {
             }
         }, utilDate.getAnoAtual(), utilDate.getMesAtual(), utilDate.getDiaAtual() );
         datePickerDialog.show();
+    }
+
+    public void escolherHoraInicialButtonAction(View view) {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                horaSelecionadaTextView.setText("" + hourOfDay + ":" + minute);
+            }
+        }, UtilTime.getHoraAtual(), UtilTime.getMinutoAtual(), true);
+        timePickerDialog.show();
+    }
+
+    public void escolherHoraFinalButtonAction(View view) {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                horaSelecionadaTextView.setText("" + hourOfDay + ":" + minute);
+            }
+        }, UtilTime.getHoraAtual(), UtilTime.getMinutoAtual(), true);
+        timePickerDialog.show();
     }
 }
