6f5e8633bcc29ca9853a778bd54a71207bfae868
BRADDTELES <braddteles@gmail.com>
2024-09-10 14:39:58 -0300

    inserindo arquivos e pasta Persistencia e o UtilSharedPreferences na pasta Util do SHARED_PREFERENCES

---
+++ app/src/main/java/com/example/appeventos/persistencia/Persistencia.java
@@ -0,0 +1,9 @@
+package com.example.appeventos.persistencia;
+
+import android.content.Context;
+
+public class Persistencia {
+    public static void setUsuario(Context context, String usuario) {
+
+    }
+}
---
+++ app/src/main/java/com/example/appeventos/util/UtilSharedPreferences.java
@@ -0,0 +1,20 @@
+package com.example.appeventos.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class UtilSharedPreferences {
+    private static final String PREFERENCES_NAME = "MyPreferences";
+
+    public static void setString(Context context, String key, String value) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getString(Context context, String key, String defaultValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(key, defaultValue);
+    }
+}
