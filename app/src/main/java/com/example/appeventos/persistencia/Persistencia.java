ac71d7369eb47444340898fac34bdcff650078bf
BRADDTELES <braddteles@gmail.com>
2024-09-10 15:29:21 -0300

    inserindo códigos na Persistencia do SHARED_PREFERENCES

--- app/src/main/java/com/example/appeventos/persistencia/Persistencia.java
+++ app/src/main/java/com/example/appeventos/persistencia/Persistencia.java
@@ -2,8 +2,24 @@ package com.example.appeventos.persistencia;
 
 import android.content.Context;
 
+import com.example.appeventos.ENUM.DEFAULT_VALUES;
+import com.example.appeventos.ENUM.SHARED_PREFERENCES;
+import com.example.appeventos.util.UtilSharedPreferences;
+
 public class Persistencia {
     public static void setUsuario(Context context, String usuario) {
+        UtilSharedPreferences.setString(context, SHARED_PREFERENCES.USUARIO_KEY.key, usuario);
+    }
 
+    public static String getUsuario(Context context) {
+        return UtilSharedPreferences.getString(context, SHARED_PREFERENCES.USUARIO_KEY.key, DEFAULT_VALUES.USUARIO_DEFAULT_VALUE.value);
+    }
+
+    public static void setSenha(Context context, String senha) {
+        UtilSharedPreferences.setString(context, SHARED_PREFERENCES.SENHA_KEY.key, senha);
+    }
+
+    public static String getSenha(Context context) {
+        return UtilSharedPreferences.getString(context, SHARED_PREFERENCES.SENHA_KEY.key, DEFAULT_VALUES.SENHA_DEFAULT_VALUE.value);
     }
 }
